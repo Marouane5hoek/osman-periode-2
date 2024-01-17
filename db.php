3d3e9b0de029ae9cd99ffb749ad7ac47dd323bb5
Marouane5hoek <133367032+Marouane5hoek@users.noreply.github.com>
2024-01-17 17:14:24 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -1,53 +1,56 @@
 <?php
+
 class Database {
     public $pdo;
- 
- 
- 
-public function __construct($db = "telefoonwinkel", $user="root", $pwd="", $host="localhost:3307") {
-    try {
-        $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
- 
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "connected to database $db";
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+
+    public function __construct($database = "rugby", $user = "root", $pwd="Nederlands", $host="localhost:3306") {
+        try{
+            $this->pdo = new PDO("mysql:host=$host;dbname=$database", $user, $pwd);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
     }
-}
- 
-public function telefoons($merk, $model, $opslag, $prijs) {
-   try {
-       $sql = "INSERT INTO telefoons (Merk, Model, Opslag, Prijs) VALUES (?,?,?,?)";
+    public function insertPlayers($a, $b, $c) {
+        $sql = "INSERT INTO players VALUES (null, :name, :surname, :age)";
         $stmt = $this->pdo->prepare($sql);
-       $stmt->execute([$merk, $model, $opslag, $prijs]);
- 
-    echo "New record created successfully";
-   } catch(PDOException $e) {
-       echo "Error: " . $e->getMessage();
+        $stmt->execute(['name' => $a, 'surname' => $b, 'age' => $c]);    
     }
-  }
+
+    public function selectPlayers($id = null) {
+        if($id) {
+            $stmt = $this->pdo->prepare("SELECT * FROM players WHERE id = ?");
+            $stmt->execute([$id]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        } else {
+            $stmt = $this->pdo->query("SELECT * FROM players");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+    }
+
+    public function editUser($naam, $achternaam, $leeftijd, $id) {
+        $stmt = $this->pdo->prepare("UPDATE players SET name = ?, surname = ?, age = ? WHERE id = ?");
+        $stmt->execute([$naam, $achternaam, $leeftijd, $id]);    
+    }
+
+    public function deleteUser($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM players WHERE id = ?");
+        $stmt->execute([$id]);
+    }
+
+    public function register($username, $password, $email) {
+        $stmt = $this->pdo->prepare("INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)");
+        $stmt->execute([$username, $password, $email]);
+    }
+
+    public function login($email) {
+        $stmt = $this->pdo->prepare("SELECT * FROM accounts WHERE email = ?");
+        $stmt->execute([$email]);
+        $result = $stmt->fetch();
+        return $result;
+    }
 }
-
-public function selectuser()   
-
 ?>
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-
-
-
-</body>
-</html>
- 
